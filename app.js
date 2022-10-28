ff33b70aab4726d3b085eb4934ec5e47e7f80e3c
Thural Musaibov <tural.musaibov@outlook.com>
2022-10-28 12:55:28 +0400

    create routes

--- app.js
+++ app.js
@@ -3,23 +3,60 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const Book = require('./models/book')
+
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const catalogRouter = require("./routes/catalog"); //Import routes for "catalog" area of site
 
 // Set up default mongoose connection
 const mongoose = require('mongoose')
-const dbURL = 'mongodb+srv://thural:<password>@cluster0.rlei7la.mongodb.net/local_library?retryWrites=true&w=majority'
+const dbURL = 'mongodb+srv://thural:mongoseed369@cluster0.rlei7la.mongodb.net/local_library?retryWrites=true&w=majority'
 mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true });
 // Get the default connection
 const db = mongoose.connection;
 // Bind connection to error event (to get notification of connection errors)
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
-// // create a new book and save it
-// const book = new Book({
+//Express server app
+var app = express();
 
-// })
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
 
 // // save and print the first book
+// const Book = require('./models/book')
+// // create a new book and save it
+// const book = new Book({
+//
+// })
 // const findFirstBook = async function() {
 //   // save/insert the book into books collection
 // await book.save()
@@ -44,45 +81,5 @@ db.on("error", console.error.bind(console, "MongoDB connection error:"));
 //   console.log(zip)
 // }
 // findZip()
-
-
-
-
-
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-//Express server app
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
